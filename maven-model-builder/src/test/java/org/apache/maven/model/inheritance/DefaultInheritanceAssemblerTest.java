812e1e9d5b7d15a3b206371fc18110193f83cc1f
Hervé Boutemy <hboutemy@apache.org>
2015-08-26 22:26:17 +0200

    MNG-5878 added project.directory property to configure module directory when not equals to artifactId

--- maven-model-builder/src/test/java/org/apache/maven/model/inheritance/DefaultInheritanceAssemblerTest.java
+++ maven-model-builder/src/test/java/org/apache/maven/model/inheritance/DefaultInheritanceAssemblerTest.java
@@ -88,12 +88,35 @@ public class DefaultInheritanceAssemblerTest
         try ( Reader control = new InputStreamReader( new FileInputStream( expected ), "UTF-8" );
               Reader test = new InputStreamReader( new FileInputStream( actual ), "UTF-8" ) )
         {
-
             XMLUnit.setIgnoreComments( true );
             XMLUnit.setIgnoreWhitespace( true );
             XMLAssert.assertXMLEqual( control, test );
         }
-
     }
 
+    public void testModulePathNotArtifactId()
+        throws Exception
+    {
+        Model parent = getModel( "module-path-not-artifactId-parent" );
+
+        Model child = getModel( "module-path-not-artifactId-child" );
+
+        SimpleProblemCollector problems = new SimpleProblemCollector();
+
+        assembler.assembleModelInheritance( child, parent, null, problems );
+
+        File actual = getTestFile( "target/test-classes/poms/inheritance/module-path-not-artifactId-actual.xml" );
+
+        writer.write( actual, null, child );
+
+        // check with getPom( "module-path-not-artifactId-effective" )
+        File expected = getPom( "module-path-not-artifactId-expected" );
+        try ( Reader control = new InputStreamReader( new FileInputStream( expected ), "UTF-8" );
+              Reader test = new InputStreamReader( new FileInputStream( actual ), "UTF-8" ) )
+        {
+            XMLUnit.setIgnoreComments( true );
+            XMLUnit.setIgnoreWhitespace( true );
+            XMLAssert.assertXMLEqual( control, test );
+        }
+    }
 }
